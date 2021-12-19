05570588fd8cf5a8520305ea1cfeb8d75b591e4b
Agata Piotrowska-Poszwa <agatapiotrowska6@wp.pl>
2021-12-19 18:38:38 +0100

    test case R8

--- Scripts/Rejestracja/R8/Script1638710180119.groovy
+++ Scripts/Rejestracja/R8/Script1638710180119.groovy
@@ -56,9 +56,9 @@ for (def row = 1; row <= findTestData('Rejestracja/tabR8').getRowNumbers(); row+
 
     WebUI.click(findTestObject('Page_PSTO webMessenger/input_Zarejestruj konto_login'))
 
-    WebUI.delay(1)
-
     WebUI.takeScreenshot(data + 'bug.png')
+
+    WebUI.delay(1)
 
     WebUI.click(findTestObject('Page_PSTO webMessenger - Zalogowano/img_Zarejestruj konto_iconImg'), FailureHandling.CONTINUE_ON_FAILURE)
 }
